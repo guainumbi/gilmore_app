37209d9d651f184d50a3a10e482134954dea0130
Jessica Grzanna <jgrzanna@me.com>
2017-10-16 21:57:44 +0200

    Create a new form to add references redirect to references index page create a method to add references to db

--- app/controllers/references_controller.rb
+++ app/controllers/references_controller.rb
@@ -1,4 +1,13 @@
 class ReferencesController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+    Reference.create(name: params[:name])
+    redirect_to references_path
+  end
+
 end
