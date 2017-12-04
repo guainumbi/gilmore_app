bdd650fddb70b2b94efd8ffdec43396c1be719f0
mauditecandela <candela.jimenez@gmail.com>
2017-12-04 09:56:20 +0100

    - Adds Edit/Update/Destroy methods to References Controller - Adds new Show and Edit View - Changes Index View - Creates Form partial - NOTE: Needs redesign (FE)

--- app/controllers/references_controller.rb
+++ app/controllers/references_controller.rb
@@ -1,8 +1,17 @@
 class ReferencesController < ApplicationController
   def index
+    @references = Reference.all
   end
 
   def new
+  end
+
+  def show
+    @reference = Reference.find(params[:id])
+  end
+
+  def edit
+    @reference = Reference.find(params[:id])
   end
 
   def create
@@ -10,4 +19,33 @@ class ReferencesController < ApplicationController
     redirect_to references_path
   end
 
+  def destroy
+    Reference.find(params[:id]).destroy
+    respond_to do |format|
+      format.html { redirect_to references_url, notice: 'Reference was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  # PATCH/PUT /references/1
+  # PATCH/PUT /references/1.json
+  def update
+    @reference = Reference.find(params[:id])
+    respond_to do |format|
+      if @reference.update(reference_params)
+        format.html { redirect_to @reference, notice: 'Reference was successfully updated.' }
+        format.json { render :show, status: :ok, location: @reference }
+      else
+        format.html { render :edit }
+        format.json { render json: @reference.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+    def reference_params
+      params.require(:reference).permit(:name, :category)
+    end
+ 
 end
