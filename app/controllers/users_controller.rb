41db2fffb7ed7de863b6af0e6f7350d1c558df19
mauditecandela <candela.jimenez@gmail.com>
2017-12-04 21:09:00 +0100

    - Adds new users resource - Adds index, new, create, destroy methods to users controller - Includes users_params method - Limits the resources

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all 
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    User.create!(user_params)
+    redirect_to users_path
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_path
+  end
+
+  private
+    
+    def user_params 
+      params.require(:user).permit(:name, :email)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users, only: [:index, :new, :create, :destroy]
   resources :episodes
   resources :references
   root 'hello#index'
