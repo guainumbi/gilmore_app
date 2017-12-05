7d1ed67b9c6f137f0cdbdfa1c9fbd8a365fbaa7b
Jessica Grzanna <jgrzanna@me.com>
2017-12-05 09:59:46 +0100

    complete routes for users resources adds edit and show actions in users controller adds edit and show view adds link_to edit and show pages in index view adds link_to new_user_path to users index view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
   def index
-    @users = User.all 
+    @users = User.all
   end
 
   def new
@@ -17,9 +21,18 @@ class UsersController < ApplicationController
     redirect_to users_path
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    User.find(params[:id]).update!(user_params)
+    redirect_to users_path
+  end
+
   private
-    
-    def user_params 
+
+    def user_params
       params.require(:user).permit(:name, :email)
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :new, :create, :destroy]
+  resources :users
   resources :episodes
   resources :references
   root 'hello#index'
